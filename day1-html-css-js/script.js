b6aa20188992c6948e73261b1198cdcdc4f3ac00
harsh <patilharsh1910@gmail.com>
2026-01-28 09:39:22 +0530

    Day 1 code

---
+++ day1-html-css-js/script.js
@@ -0,0 +1,25 @@
+console.log("Welcome to JavaScript!"); 
+
+  
+
+function greet(name) { 
+
+    console.log("Hello " + name); 
+
+} 
+
+  
+
+greet("Anmol"); 
+
+  
+
+let tasks = ["HTML", "CSS", "JavaScript"]; 
+
+  
+
+for (let i = 0; i < tasks.length; i++) { 
+
+    console.log("Task:", tasks[i]); 
+
+} 
